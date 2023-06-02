158aa3c6e23d3a4680a78eaa5a68a68176edf1be
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-06-02 18:06:55 +0200

    Fix #1 build error

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './button.css';
+import "./button.css";
 
 interface ButtonProps {
   /**
@@ -13,7 +12,7 @@ interface ButtonProps {
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   /**
    * Button contents
    */
@@ -27,21 +26,10 @@ interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ primary = false, size = "medium", backgroundColor, label, ...props }: ButtonProps) => {
+  const mode = primary ? "storybook-button--primary" : "storybook-button--secondary";
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <button type="button" className={["storybook-button", `storybook-button--${size}`, mode].join(" ")} style={{ backgroundColor }} {...props}>
       {label}
     </button>
   );
