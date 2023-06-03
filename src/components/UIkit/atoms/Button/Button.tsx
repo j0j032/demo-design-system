228d42ac5e7f144075cbae9da3a6da6f77dc264f
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-06-03 22:50:13 +0200

    🐛 | fix bad spelling

--- src/components/UIkit/atoms/Button/Button.tsx
+++ src/components/UIkit/atoms/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Button.module.scss";
+import styles from "./button.module.scss";
 import { Icon } from "../Icon/Icon";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
